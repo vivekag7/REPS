c0e3f93ec2868ad022a890060ccf1c4e5e188fb3
vgar7 <vgajadhar7@gmail.com>
2026-06-03 17:36:46 +0200

    adjusted plotting test

--- tests/testthat/test-calculate_hedonic_index.R
+++ tests/testthat/test-calculate_hedonic_index.R
@@ -343,39 +343,3 @@ test_that("chained index helpers validate period and method result shape", {
     "columns 'period' and 'Index'"
   )
 })
-
-
-test_that("Test plot_price_index", {
-  
-  # 1. Normal plotting should work silently
-  result_normal <- calculate_hedonic_index(
-    method = c("fisher", "paasche"),
-    dataset = data_constraxion,
-    period_variable = "period",
-    dependent_variable = "price",
-    numerical_variables = "floor_area",
-    categorical_variables = "neighbourhood_code",
-    reference_period = 2015
-  )
-  
-  expect_silent(plot_price_index(result_normal))
-  
-  # 2. Plotting HMTS resting_points list should trigger custom error
-  result_resting <- calculate_hedonic_index(
-    method = "hmts",
-    dataset = data_constraxion,
-    period_variable = "period",
-    dependent_variable = "price",
-    numerical_variables = "floor_area",
-    categorical_variables = "neighbourhood_code",
-    reference_period = 2015,
-    number_preliminary_periods = 2,
-    production_since = NULL,
-    resting_points = TRUE
-  )
-  
-  expect_error(
-    plot_price_index(result_resting),
-    "valid multi-method output"
-  )
-})
